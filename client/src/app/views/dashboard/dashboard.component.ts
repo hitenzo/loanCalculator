7a1577e5920cb5c49a141cdd9192312ca618a46f
hitenzo <wmowski@gmail.com>
2018-11-26 02:55:25 +0100

    input validation

--- client/src/app/views/dashboard/dashboard.component.ts
+++ client/src/app/views/dashboard/dashboard.component.ts
@@ -12,10 +12,13 @@ import { LoanCalculateResult } from '../../viewModels/loanCalculateResult';
 })
 export class DashboardComponent implements OnInit {
 
-  public LoanAmount: number;
-  public LoanDurationInYears: number;
+  public LoanAmount: string;
+  public LoanDurationInYears: string;
   public CalculatedLoan: LoanCalculateResult;
   public IsLoanCalculated: boolean = false;
+
+  public IsAmountValid: boolean = true;
+  public IsTimeValid: boolean = true;
 
   constructor(private router: Router, private loanService: LoanService) { }
 
@@ -24,24 +27,80 @@ export class DashboardComponent implements OnInit {
 
   public CalculateLoan()
   {
-    let calculateRequest = {
-      amount: this.LoanAmount,
-      durationInYears: this.LoanDurationInYears
-    }
-
-
-    this.loanService.CalculateLoan(calculateRequest).subscribe(
-      (response: LoanCalculateResult) => {
-        if (response != null) {
-          console.log(response);
-          this.CalculatedLoan = response;
-          this.IsLoanCalculated = true;
-        }
-      },
-      err => {
-        console.log(err);
+    if(this.IsAmountValid && this.IsTimeValid)
+    {
+      let calculateRequest = {
+        amount: Number(this.LoanAmount),
+        durationInYears: Number(this.LoanDurationInYears)
       }
-    );
+  
+      this.loanService.CalculateLoan(calculateRequest).subscribe(
+        (response: LoanCalculateResult) => {
+          if (response != null) {
+            console.log(response);
+            this.CalculatedLoan = response;
+            this.IsLoanCalculated = true;
+          }
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  public ValidateAmount()
+  {
+    this.FormatAmount();
+    let isNumber = !Number.isNaN(Number(this.LoanAmount));
+    let isPositive = Number(this.LoanAmount) > 0;
+
+    if(this.LoanAmount == ""){
+      this.IsAmountValid = true;
+      return;
+    }
+    else if(isNumber && isPositive) {
+      this.IsAmountValid = true;
+    }
+    else {
+      this.IsAmountValid = false;
+    }
+  }
+
+  public ValidateTime()
+  {
+    this.FormatTime();
+    let isNumber = !Number.isNaN(Number(this.LoanDurationInYears));
+    let isInRange = Number(this.LoanDurationInYears) > 0 && Number(this.LoanDurationInYears) < 100;
+
+    if(this.LoanDurationInYears == ""){
+      this.IsTimeValid = true;
+      return;
+    }
+    else if(isNumber && isInRange) {
+      this.IsTimeValid = true;
+    }
+    else {
+      this.IsTimeValid = false;
+    }
+  }
+
+  public FormatAmount()
+  {
+    if(this.LoanAmount.includes(','))
+    {
+      let frazeToReplace = /,/gi;
+      this.LoanAmount = this.LoanAmount.replace(frazeToReplace, '.');
+    }
+  }
+
+  public FormatTime()
+  {
+    if(this.LoanDurationInYears.includes(','))
+    {
+      let frazeToReplace = /,/gi;
+      this.LoanDurationInYears = this.LoanDurationInYears.replace(frazeToReplace, '.');
+    }
   }
 
 }
